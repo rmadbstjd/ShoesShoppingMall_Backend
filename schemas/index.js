f64c686e652ad48b3c496d037d38058b0b22a544
rmadbstjd <rmadbstjd@naver.com>
2023-01-21 03:47:00 +0900

    fix : localhost에서 127.0.0.1로 변경

--- schemas/index.js
+++ schemas/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connect = () => {
   mongoose
-    .connect("mongodb://localhost:27017/Shoes_ShoppingMall")
+    .connect("mongodb://127.0.0.1:27017/Shoes_ShoppingMall")
     .catch((err) => console.log(err));
 };
 
