1eb3e987f7bd6635454ec204edd831edcdfc53d5
rmadbstjd <rmadbstjd@naver.com>
2023-01-22 19:15:58 +0900

    fix: app.js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:3000",
-      "https://thriving-cheesecake-fab559.netlify.app/",
+      "https://jade-kitten-8b5131.netlify.app/",
     ], // 접근 권한을 부여하는 도메인
     credentials: true, // 응답 헤더에 Access-Control-Allow-Credentials 추가
     optionsSuccessStatus: 200, // 응답 상태 200으로 설정
@@ -24,11 +24,14 @@ app.use(
 );
 mongoose.set("strictQuery", false);
 mongoose
-  .connect("mongodb://localhost:27017", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    dbName: "Shoes_ShoppingMall",
-  })
+  .connect(
+    "mongodb+srv://shoppingMall:kys3421!@cluster0.fpbuo.mongodb.net/?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      dbName: "Shoes_ShoppingMall",
+    }
+  )
   .then(() => {
     console.log(`[+] mongoseDB Connection`);
   })
